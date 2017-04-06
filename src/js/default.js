4ced761d160951a1b978afda5ad5b85be57702c8
Andy Melien <ypm-digitalmedia@gmail.com>
2017-04-06 17:49:28 -0400

    template-ized layout with routing.  routing needs work.  TODO: mobile, subpages

--- src/js/default.js
+++ src/js/default.js
@@ -1,22 +1,127 @@
 var page = "home";
 var numRecordsAll = 10865713;
+var pageFunctions;
 
 $(document).ready(function() {
 
 
-    if (page == "home") {
-        getPhyloPics();
-        getApiStatus();
-        demoQuery();
+    // ====================================================================
+    // ======================================== Routing
+    // ====================================================================
+    var router;
+    var el = function(sel) {
+        return document.querySelector(sel);
+    }
+    var clearContent = function(containerID) {
+        $('#' + containerID).empty();
+    }
+    var setContent = function(dataID, containerID, content) {
+        // clearContent(containerID);
+        $('#' + containerID).append(content || $('#' + dataID).html());
     }
 
+    var routing = function(mode) {
 
+        router = new Navigo(null, mode === 'hash');
 
+        // var root = null;
+        // var useHash = false; // Defaults to: false
+        // var hash = '!#'; // Defaults to: '#'
+        // var router = new Navigo(root, useHash, hash);
 
+        router.on({
+            'home': function() {
+                clearContent('content-body');
+                setContent('component-headerlarge', 'content-body');
+                setContent('component-nav', 'content-body');
+                setContent('content-home', 'content-body');
+                setContent('component-nowsearching', 'content-body');
+                setContent('component-colorbars', 'content-body');
+                setContent('component-nsf', 'content-body');
+                setContent('component-footer', 'content-body');
 
+                getPhyloPics();
+                getApiStatus();
+                demoQuery();
+            },
+            'examples': function() {
+                clearContent('content-body');
+                setContent('component-headerlarge', 'content-body');
+                setContent('component-nav', 'content-body');
+                setContent('content-examples', 'content-body');
+                setContent('component-footer', 'content-body');
+            },
+            'reference': function() {
+                clearContent('content-body');
+                setContent('component-headerlarge', 'content-body');
+                setContent('component-nav', 'content-body');
+                setContent('content-reference', 'content-body');
+                setContent('component-footer', 'content-body');
+            },
+            'sandbox': function() {
+                clearContent('content-body');
+                setContent('component-headerlarge', 'content-body');
+                setContent('component-nav', 'content-body');
+                setContent('content-sandbox', 'content-body');
+                setContent('component-footer', 'content-body');
+            },
+            'about': function() {
+                clearContent('content-body');
+                setContent('component-headerlarge', 'content-body');
+                setContent('component-nav', 'content-body');
+                setContent('content-about', 'content-body');
+                setContent('component-footer', 'content-body');
+            }
+        });
+        router.on(function() {
+            clearContent('content-body');
+            setContent('component-headerlarge', 'content-body');
+            setContent('component-nav', 'content-body');
+            setContent('content-home', 'content-body');
+            setContent('component-nowsearching', 'content-body');
+            setContent('component-colorbars', 'content-body');
+            setContent('component-nsf', 'content-body');
+            setContent('component-footer', 'content-body');
+
+            getPhyloPics();
+            getApiStatus();
+            demoQuery();
+        });
+        router.resolve();
+    };
+
+    var switchModes = function() {
+        var trigger = el('.js-mode-trigger');
+        var mode = 'history-api';
+        var isLocalStorageSupported = !!window.localStorage;
+        var rerenderTrigger = function(mode) {
+            trigger.querySelector('input').checked = mode === 'hash';
+        };
+
+        if (isLocalStorageSupported) {
+            mode = localStorage.getItem('navigo') || mode;
+        }
+        rerenderTrigger(mode);
+
+        trigger.addEventListener('click', function() {
+            mode = mode === 'history-api' ? 'hash' : 'history-api';
+            isLocalStorageSupported && localStorage.setItem('navigo', mode);
+            window.location.href = (router.root || '').replace('#', '');
+            setTimeout(function() {
+                window.location.reload(true);
+            }, 200);
+        });
+
+        return mode;
+    };
+
+    var init = function() {
+        routing(switchModes());
+    };
+
+    window.onload = init;
 
 }); // end document ready
-
 
 
 
@@ -35,18 +140,22 @@ $(document).ready(function() {
 
 function getPhyloPics() {
     var numImgs = 0;
+    var sampleSize = 50;
     var max = 12;
 
     $.get("http://phylopic.org/api/a/image/count", function(num) {
 
         numImgs = num.result;
 
-        var start = _.random(0, numImgs - max);
+        var start = _.random(0, numImgs - sampleSize);
 
-        $.get("http://phylopic.org/api/a/image/list/" + start + "/" + max + "?options=uid+pngFiles+svgFile", function(data) {
+        $.get("http://phylopic.org/api/a/image/list/" + start + "/" + sampleSize + "?options=uid+pngFiles+svgFile", function(data) {
             var urls = [];
+            var dataset = data.result;
+            // console.log(dataset);
+            var data_reduced = _.sampleSize(dataset, max);
 
-            data.result.forEach(function(i) {
+            data_reduced.forEach(function(i) {
                 var url = i.pngFiles[0].url;
                 urls.push(url);
                 $("#phylopics").append("<div class='col-4 col-3-s col-1-l col-2-m'><div class='phylo'><img src='http://phylopic.org" + url + "' /></div></div>").hide().fadeIn();
